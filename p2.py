3a1056cc43f52f5de41e6f4a4a9e3e3d82195763
A030708 <abhiramkm2003@gmail.com>
2026-02-11 12:31:34 +0530

    Add sleep delay before quitting the Selenium driver
    Added a sleep delay before quitting the driver.

--- p2.py
+++ p2.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
 
 driver.find_element(By.XPATH, "//button[text()='Add Element']").click()
-
+time.sleep(5)
 driver.quit()
+
